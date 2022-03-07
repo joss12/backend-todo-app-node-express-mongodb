0b3fc46b406a2debf1613cba7f6335cc070c8573
joss12 <39594142+joss12@users.noreply.github.com>
2022-03-06 19:53:23 -0800

    Start work on Logout

--- routes/Todos.js
+++ routes/Todos.js
@@ -70,6 +70,152 @@ router.get("/current", requiresAuth, async (req, res)=>{
     }
 })
 
+//@route PUT /api/todos/:toDoId/complete
+//@desc Marke todos as complete
+//@access Private
+
+router.put("/:toDoId/complete", requiresAuth, async(req, res)=>{
+    try {
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId
+        });
+
+    if(!toDo){
+        return res.status(401).json({error: "Couldn't find Todo"})
+    }
+
+    if(toDo.complete){
+        return res.status(400).json({error: "ToDo is already completed"})
+    };
+    const upodatedTodo = await ToDo.findByIdAndUpdate(
+        {
+            user:req.user._id,
+            _id: req.params.toDoId,
+        },{
+            complete: true,
+            completedAt: new Date(),
+        },{
+            new: true
+        }
+        );
+        return res.json(upodatedTodo)
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error.message)
+    }
+});
+
+//@route PUT /api/todos/:toDoId/incomplete
+//@desc Marke todos as incomplete
+//@access Private
+
+router.put("/:toDoId/incomplete", requiresAuth, async(req, res)=>{
+    try {
+        const toDo = ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+
+        if(!toDo){
+            return res.status(404).json({error: "Could not find Todo"})
+        }
+
+        if(!toDo.complete){
+            return res.status(400).json({error: "ToDo is already incomplte"});
+        }
+
+        const upodatedTodo = await ToDo.findByIdAndUpdate(
+            {
+                user: req.user._id,
+                _id: req.params.toDoId,
+            },{
+                complete: false,
+                completedAt: null
+            },{
+                new: true
+            }
+        )
+
+        return res.json(upodatedTodo);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error.message);
+        
+    }
+});
+
+//@route PUT /api/todos/:toDoId
+//@desc Updtae a todo
+//@access Private
+
+
+router.put("/:toDoId", requiresAuth, async (req, res)=>{
+    try {
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId
+        });
+
+        if(!toDo){
+            return res.status(404).json({error: "Could not find ToDo"});
+        }
+
+        const {isValid, errors} = validateTodoInput(req.body);
+
+        if(!isValid){
+            return res.status(400).json(errors);
+
+        }
+        const upodatedTodo = await ToDo.findByIdAndUpdate(
+            {
+                user: req.user._id,
+                _id: req.params.toDoId,
+            },{
+                content: req.body.content,
+            },
+            {
+                new: true
+            }
+        );
+
+        return res.json(upodatedTodo)
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error.message)
+    }
+})
+
+//@route Delete /api/todos/:toDoId
+//@desc Delete a todo
+//@access Private
+
+router.delete("/:toDoId", requiresAuth, async(req, res)=>{
+    try {
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+        if(!toDo){
+            return res.status(400).json({error: "Could not find Todo"});
+        }
+        
+        await ToDo.findOneAndRemove({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+
+        return res.json({success: true})
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error.message)
+    }
+})
+
 module.exports = router;
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -53,6 +53,18 @@ router.post("/register", async(req, res)=>{
         //save user to the database
         const savedUser = await newUser.save();
 
+        const payload = {userId: savedUser._id};
+
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "7d"
+            });
+
+            res.cookie("access-token", token, {
+                expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production"
+            });
+
         //
         const userToReturn = {...savedUser._doc}
         delete userToReturn.password;
@@ -77,7 +89,7 @@ router.post("/login", async (req, res)=>{
         //check for the user
         const user = await User.findOne({
             email: new RegExp("^" + req.body.email + "$", "i")
-        })
+        });
 
         if(!user){
             return res.status(400).json({error: 'There was a problem with your login credentials'})
@@ -129,7 +141,27 @@ router.get("/current", requiresAuth, (req, res)=>{
     }
 
     return res.json(req.user)
-})
+});
+
+
+
+//@route PUT/api/auth/logout
+//@desc Logout user
+//@access Prive
+
+router.put("/logout", requiresAuth, async(req, res)=>{
+
+    try {
+        res.clearCookie("access-token");
+
+        return res.json({success: true})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(err.message)
+        
+    }
+
+});
 
 
 module.exports = router
